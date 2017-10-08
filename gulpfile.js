48bf175b298e3d49358c361a8a8df3a859a35259
Zahari Dimitrov <zach_dimitrov@yahoo.com>
2017-10-08 21:36:54 +0300

    update gulpfile build

--- gulpfile.js
+++ gulpfile.js
@@ -1,51 +1,79 @@
 const gulp = require('gulp');
 const eslint = require('gulp-eslint');
 const csslint = require('gulp-csslint');
-// const gulpsync = require('gulp-sync')(gulp);
+const gulpsync = require('gulp-sync')(gulp);
 const babel = require('gulp-babel');
-// const nodemon = require('gulp-nodemon');
-const clean = require('gulp-clean');
+const del = require('del');
+const cssMin = require('gulp-css');
+const smushit = require('gulp-smushit');
+const nodemon = require('gulp-nodemon');
 
-gulp.task('default', () => {});
+gulp.task('default', gulpsync.sync(['build', 'start']));
 
 // lint
 
-gulp.task('lint', ['lint:js', 'lint:css']);
-
 gulp.task('lint:js', () => {
-    return gulp.src(['**/*.js', '!node_modules/**'])
+    return gulp.src(['src/**/*.js', '!node_modules/**'])
         .pipe(eslint())
         .pipe(eslint.format())
         .pipe(eslint.failAfterError());
 });
 
 gulp.task('lint:css', () => {
-    return gulp.src('static/css/*.css')
+    return gulp.src('src/**/*.css')
         .pipe(csslint())
         .pipe(csslint.formatter());
 });
 
-// build
+gulp.task('lint', ['lint:js', 'lint:css']);
 
-gulp.task('build', ['clean', 'lint', 'compile']);
+// clean
 
 gulp.task('clean', () => {
-    return gulp.src('build', { read: false })
-        .pipe(clean());
+    return del('build/');
 });
 
-gulp.task('compile', ['compile:js']);
+// compile
 
 gulp.task('compile:js', () => {
-    gulp.src(['**/*.js', '!node_modules/**', '!_design/'])
+    return gulp.src(['src/**/*.js', '!node_modules/**', '!_design/'])
         .pipe(babel({
             presets: ['env'],
         }))
-        .pipe(gulp.dest('build'));
+        .pipe(gulp.dest('build/'));
 });
+
+gulp.task('compile:css', () => {
+    return gulp.src('src/**/*.css')
+        .pipe(cssMin())
+        .pipe(gulp.dest('build/'));
+});
+
+gulp.task('compile:img', () => {
+    return gulp.src(['src/**/images/*', '!players/'])
+        .pipe(smushit())
+        .pipe(gulp.dest('build/'));
+});
+
+gulp.task('compile', ['compile:js', 'compile:css', 'compile:img']);
+
+// copy
+
+gulp.task('copy', () => {
+    return gulp.src(['src/**/*.html', 'src/**/*.handlebars'])
+        .pipe(gulp.dest('build/'));
+});
+
+// build
+
+gulp.task('build', gulpsync.sync(['clean', 'lint', 'compile', 'copy']));
 
 // start
 
-gulp.task('server:start', () => {
-    return require('./server');
+gulp.task('start', () => {
+    nodemon({
+        script: 'build/server.js',
+        ext: 'js html',
+        env: { 'NODE_ENV': 'development' },
+    });
 });
