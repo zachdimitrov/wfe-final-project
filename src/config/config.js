b495abd28eed8cb81470c8e9c503205842045358
Zahari Dimitrov <zach_dimitrov@yahoo.com>
2017-10-09 10:05:35 +0300

    changes in configuration form build

--- src/config/config.js
+++ src/config/config.js
@@ -1,5 +1,9 @@
 const port = 3003;
-const connectionString = 'mongodb://localhost/tennis-vission-db';
+const connectionString = {
+    default: 'mongodb://localhost/tennis-vission-db',
+    deploy: 'mongodb://admin:telerikapp1@ds013405.mlab.com:13405/tennis-vission',
+};
+
 const sessionSecret = 'Purple Unicorn';
 
 module.exports = { port, connectionString, sessionSecret };
