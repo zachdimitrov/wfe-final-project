792ee929bad68c3afe5c049cb87ae910eb2f07c3
Zahari Dimitrov <zach_dimitrov@yahoo.com>
2017-10-09 13:44:31 +0300

    changes in connection string

--- src/config/config.js
+++ src/config/config.js
@@ -1,8 +1,8 @@
 const port = 3003;
 const connectionString = {
     default: 'mongodb://localhost/tennis-vission-db',
-    // 'mongodb://admin:telerikapp1@ds013405.mlab.com:13405/tennis-vission'
-    deploy: 'mongodb://admin:telerikapp1@ds013405.mlab.com:13405/heroku_br99x7gq',
+    deploy: 'mongodb://admin:telerikapp1@ds013405.mlab.com:13405/tennis-vission',
+    // deploy: 'mongodb://admin:telerikapp1@ds013405.mlab.com:13405/heroku_br99x7gq',
 };
 
 const sessionSecret = 'Purple Unicorn';
