0e327a16ab11ecf73f5ac0def8254538e1acf4e8
Zahari Dimitrov <zach_dimitrov@yahoo.com>
2017-10-09 15:39:36 +0300

    make it better

--- build/config/config.js
+++ build/config/config.js
@@ -4,7 +4,7 @@
 /* eslint-disable no-process-env */
 
 var port = process.env.PORT || 5000;
-var uri = process.env.MONGOLAB_URI;
+var uri = process.env.MONGOLAB_YELLOW_URI;
 var connectionString = {
     default: 'mongodb://localhost/tennis-vission-db',
     // deploy: 'mongodb://admin:telerikapp1@ds013405.mlab.com:13405/tennis-vission',
