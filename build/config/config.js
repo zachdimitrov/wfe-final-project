2f5d7f94edfa7514c5285886e44b0aef77645b28
Zahari Dimitrov <zach_dimitrov@yahoo.com>
2017-10-09 15:15:33 +0300

    make it better

--- build/config/config.js
+++ build/config/config.js
@@ -1,10 +1,14 @@
 'use strict';
 
-var port = 3003;
+/* globals process */
+/* eslint-disable no-process-env */
+
+var port = process.env.PORT || 5000;
 var connectionString = {
     default: 'mongodb://localhost/tennis-vission-db',
-    deploy: 'mongodb://admin:telerikapp1@ds013405.mlab.com:13405/tennis-vission'
+    // deploy: 'mongodb://admin:telerikapp1@ds013405.mlab.com:13405/tennis-vission',
     // deploy: 'mongodb://admin:telerikapp1@ds013405.mlab.com:13405/heroku_br99x7gq',
+    deploy: 'mongodb://admin:telerikapp1@ds113505.mlab.com:13505/heroku_rlflgqcx'
 };
 
 var sessionSecret = 'Purple Unicorn';
--- src/config/config.js
+++ src/config/config.js
@@ -4,8 +4,9 @@
 const port = process.env.PORT || 5000;
 const connectionString = {
     default: 'mongodb://localhost/tennis-vission-db',
-    deploy: 'mongodb://admin:telerikapp1@ds013405.mlab.com:13405/tennis-vission',
+    // deploy: 'mongodb://admin:telerikapp1@ds013405.mlab.com:13405/tennis-vission',
     // deploy: 'mongodb://admin:telerikapp1@ds013405.mlab.com:13405/heroku_br99x7gq',
+    deploy: 'mongodb://admin:telerikapp1@ds113505.mlab.com:13505/heroku_rlflgqcx',
 };
 
 const sessionSecret = 'Purple Unicorn';
