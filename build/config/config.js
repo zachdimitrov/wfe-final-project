f1b4c6e02d3ec8614a71925443575ccb25d920f9
Zahari Dimitrov <zach_dimitrov@yahoo.com>
2017-10-09 15:33:01 +0300

    make it better

--- build/config/config.js
+++ build/config/config.js
@@ -4,11 +4,13 @@
 /* eslint-disable no-process-env */
 
 var port = process.env.PORT || 5000;
+var uri = process.env.MONGOLAB_URI;
 var connectionString = {
     default: 'mongodb://localhost/tennis-vission-db',
     // deploy: 'mongodb://admin:telerikapp1@ds013405.mlab.com:13405/tennis-vission',
     // deploy: 'mongodb://admin:telerikapp1@ds013405.mlab.com:13405/heroku_br99x7gq',
-    deploy: 'mongodb://admin:telerikapp1@ds113505.mlab.com:13505/heroku_rlflgqcx'
+    // deploy: 'mongodb://admin:telerikapp1@ds113505.mlab.com:13505/heroku_rlflgqcx',
+    deploy: uri
 };
 
 var sessionSecret = 'Purple Unicorn';
